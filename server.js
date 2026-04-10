5d3b4825eae9512059bac059d071cf73df49e334
info@traulog.com <info@traulog.com>
2026-04-10 17:07:36 -0400

    fix puerto render

--- server.js
+++ server.js
@@ -66,6 +66,8 @@ app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
